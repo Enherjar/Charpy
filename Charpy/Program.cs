2b51c573b56fda1d27c7f6464591ba5c5f0546a0
Enherjar <valentin_adi@yahoo.com>
2023-03-13 15:28:05 +0200

    Homework

--- Charpy/Program.cs
+++ Charpy/Program.cs
@@ -1,6 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int smt 
-    int a= 0;
-int b = 2;
-smt = a + b;
+﻿//Homework Day1
+
+int A = 5;
+double B = 6.99;
+double Sum, Subb, Mult, Div, Mod;
+
+Sum =  A + B;
+Subb = A - B;
+Mult =  A * B;
+Div = A / B;
+Mod = A % B;
+Console.WriteLine(Sum);
+Console.WriteLine(Subb);
+Console.WriteLine(Mult);
+Console.WriteLine(Div);
+Console.WriteLine(Mod);
+Console.WriteLine(Sum++);
+Console.WriteLine(Subb--);
+
